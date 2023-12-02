b66c54671fd191a942e5c6948f621dec3e89b974
Alec Merdler <alecmerdler@gmail.com>
2023-12-02 15:18:46 -0500

    day 2, part 1 first attempt

---
+++ day2/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CubeColor string
+
+const (
+	red   CubeColor = "red"
+	green CubeColor = "green"
+	blue  CubeColor = "blue"
+)
+
+var cubeColors = []CubeColor{red, green, blue}
+
+type Grab map[CubeColor]int
+
+type Game struct {
+	ID    int
+	Grabs []Grab
+}
+
+var maxForColor = map[CubeColor]int{
+	red:   12,
+	green: 13,
+	blue:  14,
+}
+
+func gameFrom(id int, line string) Game {
+	game := Game{ID: id, Grabs: []Grab{}}
+
+	grabsOnly := strings.Split(line, ":")[1]
+	grabsRaw := strings.Split(grabsOnly, ";")
+	for _, grabRaw := range grabsRaw {
+		grab := Grab{}
+
+		colors := strings.Split(grabRaw, ",")
+		for _, color := range colors {
+			for _, cubeColor := range cubeColors {
+				if strings.HasSuffix(color, string(cubeColor)) {
+					number := strings.TrimSpace(strings.ReplaceAll(color, string(cubeColor), ""))
+					grabbed, err := strconv.Atoi(number)
+					if err != nil {
+						panic(err)
+					}
+
+					grab[cubeColor] = grabbed
+					break
+				}
+			}
+		}
+
+		game.Grabs = append(game.Grabs, grab)
+	}
+
+	return game
+}
+
+func partOne(lines []string) int {
+	possibleGames := []Game{}
+
+	for index, line := range lines {
+		id := index + 1
+		game := gameFrom(id, line)
+		possible := true
+
+		for _, grab := range game.Grabs {
+			if !possible {
+				break
+			}
+
+			for color, numPulled := range grab {
+				if numPulled > maxForColor[color] {
+					fmt.Printf("\nGame %d not possible: found %d %s", id, numPulled, string(color))
+					possible = false
+					break
+				}
+			}
+		}
+
+		if possible {
+			possibleGames = append(possibleGames, game)
+		}
+	}
+
+	total := 0
+	for _, possibleGame := range possibleGames {
+		fmt.Printf("\nGame %d possible, total = %d", possibleGame.ID, total)
+		total += possibleGame.ID
+	}
+
+	return total
+}
+
+func main() {
+	inputRaw, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	file := string(inputRaw)
+	lines := strings.Split(file, "\n")
+
+	partOneAnswer := partOne(lines)
+
+	fmt.Printf("\nPart one answer: %d\n", partOneAnswer)
+}
